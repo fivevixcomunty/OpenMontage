fdcf2f58d24cfee03eff8f5dbf70ceec389f9b25
Ayberk0 <turantasayberk@gmail.com>
2026-09-08 17:49:02 +0300

    Fix syntax error on line 253 in ShortsComposition

--- remotion-composer/src/ShortsComposition.tsx
+++ remotion-composer/src/ShortsComposition.tsx
@@ -250,7 +250,7 @@ const RetentionBar: React.FC<ShortsCompositionProps> = ({
         style={{
           position: "absolute",
           left: "50%",
-          top: 50%,
+          top: "50%",
           transform: "translate(-50%, -50%)",
           color: "#FFFFFF",
           fontFamily: "'Montserrat Medium', sans-serif",
